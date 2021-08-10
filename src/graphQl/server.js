e76c6c6714ee444ab8be5b47c35a00391320aa65
marawanthedev <tmarawan11@gmail.com>
2021-08-10 08:37:34 +0200

    switched for hosted backend to deploy

--- src/graphQl/server.js
+++ src/graphQl/server.js
@@ -1,8 +1,8 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/",
-  cache: new InMemoryCache()
-});
+  uri: 'https://ecommerce-app-server-apollo.herokuapp.com/',
+  cache: new InMemoryCache(),
+})
 
-export default client;
+export default client
